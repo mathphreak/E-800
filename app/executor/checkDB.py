5c3962c3e70dbd965aaba9f5204f4e0015bb52b7
Matt Horn <mathphreak@gmail.com>
2016-01-07 16:08:44 -0600

    Store results of running submission

--- app/executor/checkDB.py
+++ app/executor/checkDB.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import psycopg2
-from subprocess import call
+import subprocess
+import os
 conn = psycopg2.connect("host=db dbname=postgres user=postgres")
+conn.autocommit = True
 cur = conn.cursor()
 cur.execute("""
     SELECT submissions.id, submissions.code, assignments.run_script
@@ -11,12 +13,25 @@ cur.execute("""
     WHERE submissions.pending;
 """)
 (id, code, script) = cur.fetchone()
+cur.close()
 with open("/root/e800_run.sh", "w") as script_file:
     script_file.write(script)
     script_file.write("\n")
+
 with open("/root/code.txt", "w") as code_file:
     code_file.write(code)
     code_file.write("\n")
-call(["sh", "/root/e800_run.sh"])
+
+output = subprocess.check_output(["sh", "/root/e800_run.sh"], stderr=subprocess.STDOUT)
+cur = conn.cursor()
+cur.execute("""
+    UPDATE submissions
+    SET
+        output = %(output)s,
+        pending = false
+    WHERE id = %(id)s;
+""", {'output': output, 'id': id})
 cur.close()
 conn.close()
+os.remove("/root/e800_run.sh")
+os.remove("/root/code.txt")
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014175842) do
+ActiveRecord::Schema.define(version: 20160107213524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151014175842) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.boolean  "pending",       default: true
+    t.text     "output"
   end
 
   add_index "submissions", ["assignment_id"], name: "index_submissions_on_assignment_id", using: :btree
