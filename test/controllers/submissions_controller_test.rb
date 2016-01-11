c1d39ee594faa6d27c579ccf12cbad2f793fd162
Matt Horn <mathphreak@gmail.com>
2016-01-11 16:57:34 -0600

    Test the Submissions controller

--- test/controllers/submissions_controller_test.rb
+++ test/controllers/submissions_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class SubmissionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create submission" do
+    assignment = create :assignment
+    assert_difference('assignment.submissions.count') do
+      post :create, assignment_id: assignment.id,
+                    submission: {author: 'Test',
+                                 code: 'nothing'}
+    end
+
+    assert_redirected_to assignment_path(assignment)
+  end
+
+  test "should destroy submission" do
+    submission = create :submission
+    assignment = submission.assignment
+    assert_difference('assignment.submissions.count', -1) do
+      delete :destroy, assignment_id: assignment.id,
+                       id: submission.id
+    end
+
+    assert_redirected_to assignment_path(assignment)
+  end
 end
