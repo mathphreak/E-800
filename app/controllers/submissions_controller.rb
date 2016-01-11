855bf420a941bb7379b9f2738bdbff4a6376c3d2
Matt Horn <mathphreak@gmail.com>
2016-01-11 16:58:09 -0600

    Don't expose the list of all pending submissions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,19 +2,6 @@
 require_relative '../jobs/submissions_execute_job'
 
 class SubmissionsController < ApplicationController
-  def pending_index
-    @assignments = Assignment.all
-    @submissions = @assignments
-                   .map(&:submissions)
-                   .flatten
-                   .select(&:pending)
-    respond_to do |format|
-      format.html
-      format.xml  { render xml: @submissions }
-      format.json { render json: @submissions }
-    end
-  end
-
   def create
     @assignment = Assignment.find(params[:assignment_id])
     @submission = @assignment.submissions.create(submission_params)
