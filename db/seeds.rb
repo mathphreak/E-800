84815d01795dd920739fa7bcfe561675e3678728
Matt Horn <mathphreak@gmail.com>
2016-09-17 18:49:56 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Assignment.create(title: "Hello World",
+  description: "Write a Bash script that prints 'hello world'.",
+  run_script: "#!/bin/bash\n\nbash helloworld.sh")
